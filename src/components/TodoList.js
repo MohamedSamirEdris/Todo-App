5e2dd1f9389c9b7f6a97ced6ad1e14fbff4bd4a3
mohamedsamir <mohamedsamiredries@outlook.com>
2023-07-09 11:33:32 +0300

    Add edit to do

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,16 +1,29 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
-import { MdDelete } from "react-icons/md";
+import { MdDelete, MdEdit } from "react-icons/md"; // Import MdEdit from react-icons/md
 import { useDispatch } from 'react-redux';
-import { deleteTodos } from '../reduxStore/TodoSlice';
+import { deleteTodos, editTodo } from '../reduxStore/TodoSlice'; // Import editTodo action
 import { Checkbox } from 'antd';
 
 const TodoList = ({ todo, _id }) => {
   const dispatch = useDispatch();
   const [done, setDone] = useState(false);
+  const [editing, setEditing] = useState(false); // Add editing state
+  const [editedTodo, setEditedTodo] = useState(todo); // Add editedTodo state
 
   const handleTodoRemove = () => {
     dispatch(deleteTodos(_id));
+  };
+
+  const handleTodoEdit = () => {
+    if (editing) {
+      dispatch(editTodo({ _id, todo: editedTodo }));
+    }
+    setEditing(!editing);
+  };
+
+  const handleTodoInputChange = (e) => {
+    setEditedTodo(e.target.value);
   };
 
   return (
@@ -25,10 +38,25 @@ const TodoList = ({ todo, _id }) => {
       } w-full font-tiitleFont font-medium text-base border-[1px] border-l-[6px]  px-2 py-1 cursor-pointer flex items-center justify-between`}
     >
       <Checkbox checked={done} onChange={(e) => setDone(e.target.checked)} />
-      <span>{todo}</span>
-      <span onClick={handleTodoRemove} className="text-xl text-gray-300 hover:text-red-500 duration-300 cursor-pointer">
-        <MdDelete />
-      </span>
+      {editing ? (
+        <input
+          type="text"
+          value={editedTodo}
+          onChange={handleTodoInputChange}
+          onBlur={handleTodoEdit} // Save changes on blur
+          autoFocus // Automatically focus on input when editing starts
+        />
+      ) : (
+        <span>{todo}</span>
+      )}
+      <div>
+        <span onClick={handleTodoEdit} className="text-xl text-gray-300 hover:text-blue-500 duration-300 cursor-pointer">
+          {editing ? <MdEdit /> : <MdEdit />}
+        </span>
+        <span onClick={handleTodoRemove} className="text-xl text-gray-300 hover:text-red-500 duration-300 cursor-pointer">
+          <MdDelete />
+        </span>
+      </div>
     </motion.li>
   );
 }
--- src/reduxStore/TodoSlice.js
+++ src/reduxStore/TodoSlice.js
@@ -1,24 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  todosList:[]
+  todosList: []
 };
 
 export const todoSlice = createSlice({
-    name:"todos",
-    initialState,
-        reducers:{
-            addTodos:(state,action)=>{
-                state.todosList.push(action.payload)
-            },
-            deleteTodos:(state,action)=>{
-                state.todosList = state.todosList.filter((item)=>item._id !==action.payload)
-            },
-            removeTodos:(state)=>{
-                state.todosList =[];
-            }
+  name: "todos",
+  initialState,
+  reducers: {
+    addTodos: (state, action) => {
+      state.todosList.push(action.payload);
+    },
+    deleteTodos: (state, action) => {
+      state.todosList = state.todosList.filter((item) => item._id !== action.payload);
+    },
+    editTodo: (state, action) => {
+      const { _id, todo } = action.payload;
+      const todoIndex = state.todosList.findIndex((item) => item._id === _id);
+      if (todoIndex !== -1) {
+        state.todosList[todoIndex].todo = todo;
+      }
+    },
+    removeTodos: (state) => {
+      state.todosList = [];
     }
-})
+  }
+});
 
-export const {addTodos, deleteTodos,removeTodos} = todoSlice.actions;
-export default todoSlice.reducer
+export const { addTodos, deleteTodos, editTodo, removeTodos } = todoSlice.actions;
+export default todoSlice.reducer;
